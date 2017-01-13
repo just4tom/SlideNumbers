e1c59f06d478aa86f7b2893fc45a19c82d14b15c
just4tom <304871153@qq.com>
2017-01-13 12:04:50 +0800

    Slide Numbers 1.0
    a small game code by c#

---
+++ MainForm.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnRandom = new System.Windows.Forms.Button();
+            this.btnInitialize = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.设置ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.x3ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.x4ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.x5ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tableLayoutPanel1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(69, 38);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(334, 299);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // btnRandom
+            // 
+            this.btnRandom.Location = new System.Drawing.Point(99, 352);
+            this.btnRandom.Name = "btnRandom";
+            this.btnRandom.Size = new System.Drawing.Size(87, 35);
+            this.btnRandom.TabIndex = 1;
+            this.btnRandom.Text = "Random";
+            this.btnRandom.UseVisualStyleBackColor = true;
+            this.btnRandom.Click += new System.EventHandler(this.btnRandom_Click);
+            // 
+            // btnInitialize
+            // 
+            this.btnInitialize.Location = new System.Drawing.Point(286, 352);
+            this.btnInitialize.Name = "btnInitialize";
+            this.btnInitialize.Size = new System.Drawing.Size(87, 35);
+            this.btnInitialize.TabIndex = 1;
+            this.btnInitialize.Text = "Initialize";
+            this.btnInitialize.UseVisualStyleBackColor = true;
+            this.btnInitialize.Click += new System.EventHandler(this.btnInitialize_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.设置ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(493, 25);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 设置ToolStripMenuItem
+            // 
+            this.设置ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.x3ToolStripMenuItem,
+            this.x4ToolStripMenuItem,
+            this.x5ToolStripMenuItem});
+            this.设置ToolStripMenuItem.Name = "设置ToolStripMenuItem";
+            this.设置ToolStripMenuItem.Size = new System.Drawing.Size(60, 21);
+            this.设置ToolStripMenuItem.Text = "Option";
+            // 
+            // x3ToolStripMenuItem
+            // 
+            this.x3ToolStripMenuItem.Checked = true;
+            this.x3ToolStripMenuItem.CheckOnClick = true;
+            this.x3ToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.x3ToolStripMenuItem.Name = "x3ToolStripMenuItem";
+            this.x3ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.x3ToolStripMenuItem.Text = "3X3";
+            this.x3ToolStripMenuItem.Click += new System.EventHandler(this.x3ToolStripMenuItem_Click);
+            // 
+            // x4ToolStripMenuItem
+            // 
+            this.x4ToolStripMenuItem.CheckOnClick = true;
+            this.x4ToolStripMenuItem.Name = "x4ToolStripMenuItem";
+            this.x4ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.x4ToolStripMenuItem.Text = "4X4";
+            this.x4ToolStripMenuItem.Click += new System.EventHandler(this.x4ToolStripMenuItem_Click);
+            // 
+            // x5ToolStripMenuItem
+            // 
+            this.x5ToolStripMenuItem.CheckOnClick = true;
+            this.x5ToolStripMenuItem.Name = "x5ToolStripMenuItem";
+            this.x5ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.x5ToolStripMenuItem.Text = "5X5";
+            this.x5ToolStripMenuItem.Click += new System.EventHandler(this.x5ToolStripMenuItem_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(493, 399);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.btnInitialize);
+            this.Controls.Add(this.btnRandom);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "MainForm";
+            this.Text = "SlideNumbers";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button btnRandom;
+        private System.Windows.Forms.Button btnInitialize;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 设置ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem x3ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem x4ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem x5ToolStripMenuItem;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class MainForm : Form
+    {
+        int nRowColLen = 3;  //设置游戏每行（列）的格子数
+        Label emptyLabel;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            InitializeLayOut(nRowColLen);            
+        }
+
+        private void InitializeLayOut(int nLenIn)
+        {
+            tableLayoutPanel1.Controls.Clear();
+            //comboBox1.SelectedIndex = 0; //3X3
+            int nLen = nLenIn;
+            Single sPercent = 100 / nLen;
+
+            this.tableLayoutPanel1.ColumnCount = nLen;
+            this.tableLayoutPanel1.RowCount = nLen;
+            for (int i = 0; i < nLen; i++)
+            {
+                this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, sPercent));
+                this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, sPercent));
+            }
+            for (int i = 0; i < nLen; i++)
+            {
+                for (int j = 0; j < nLen; j++)
+                {
+                    System.Windows.Forms.Label theLabel = new Label();
+                    theLabel.AutoSize = true;
+                    theLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+                    theLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+                    theLabel.Location = new System.Drawing.Point(1, 1);
+                    theLabel.Margin = new System.Windows.Forms.Padding(1);
+                    theLabel.Name = "label1";
+                    theLabel.Size = new System.Drawing.Size(88, 79);
+                    theLabel.TabIndex = 0;
+                    theLabel.Text = (nLen * i + j + 1).ToString();
+                    theLabel.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+                    theLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+                    theLabel.Tag = i.ToString() + "," + j.ToString();
+                    theLabel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.lable_MouseDown);
+                    this.tableLayoutPanel1.Controls.Add(theLabel, j, i);
+                }
+            }
+
+            Label lastLabel = (Label)tableLayoutPanel1.GetControlFromPosition(nLen - 1, nLen - 1);
+            lastLabel.Text = "";
+            emptyLabel = lastLabel;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //InitializeLayOut(comboBox1.SelectedIndex);
+        }
+
+        private void x3ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            nRowColLen = 3;
+            x3ToolStripMenuItem.Checked = true;
+            x4ToolStripMenuItem.Checked = false;
+            x5ToolStripMenuItem.Checked = false;
+            InitializeLayOut(nRowColLen);
+        }
+
+        private void x4ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            nRowColLen = 4;
+            x3ToolStripMenuItem.Checked = false;
+            x5ToolStripMenuItem.Checked = false;
+            InitializeLayOut(nRowColLen);
+        }
+
+        private void x5ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            nRowColLen = 5;
+            x3ToolStripMenuItem.Checked = false;
+            x4ToolStripMenuItem.Checked = false;
+            InitializeLayOut(nRowColLen);
+        }
+
+        private void lable_MouseDown(object sender, EventArgs e)
+        {
+            string[] strLabelPos = ((Label)sender).Tag.ToString().Split(',');
+            string[] strEmptyLabelPos = emptyLabel.Tag.ToString().Split(',');
+            int labelRow = Convert.ToInt32(strLabelPos[0]);
+            int labelCol = Convert.ToInt32(strLabelPos[1]);
+            int emptyLabelRow = Convert.ToInt32(strEmptyLabelPos[0]);
+            int emptyLabelCol = Convert.ToInt32(strEmptyLabelPos[1]);
+
+            if(labelRow == emptyLabelRow && labelCol != emptyLabelCol)
+            {
+                int colCount = emptyLabelCol;               
+                do 
+                {
+                    if(labelCol > emptyLabelCol)
+                    {
+                        colCount++;
+                    }
+                    else
+                    {
+                        colCount--;
+                    }
+                    Label toMoveLabel = (Label)tableLayoutPanel1.GetControlFromPosition(colCount, emptyLabelRow);
+                    emptyLabel.Text = toMoveLabel.Text;
+                    toMoveLabel.Text = "";
+                    emptyLabel = toMoveLabel;
+
+                } while (colCount != labelCol);
+            }
+            else if(labelCol == emptyLabelCol && labelRow != emptyLabelRow)
+            {
+                int rowCount = emptyLabelRow;
+                do 
+                {
+                    if(labelRow > emptyLabelRow)
+                    {
+                        rowCount++;
+                    }
+                    else
+                    {
+                        rowCount--;
+                    }
+                    Label toMoveLabel = (Label)tableLayoutPanel1.GetControlFromPosition(emptyLabelCol, rowCount);
+                    emptyLabel.Text = toMoveLabel.Text;
+                    toMoveLabel.Text = "";
+                    emptyLabel = toMoveLabel;
+                } while (rowCount != labelRow);
+            }
+
+            if(judgeSuccess())
+            {
+                MessageBox.Show("Congratulations! You Win!");
+            }
+        }
+
+        private bool judgeSuccess()
+        {
+            int valueCount = 0;
+            for (int i = 0; i < nRowColLen; i++)
+            {
+                for (int j = 0; j < nRowColLen; j++)
+                {
+                    if(i==nRowColLen-1 && j==nRowColLen-1)
+                    {
+                        continue;
+                    }
+
+                    valueCount++;
+                    Label judgeLabel = (Label)tableLayoutPanel1.GetControlFromPosition(j, i);
+                    if(judgeLabel.Text != "")
+                    {
+                        if (Convert.ToInt32(judgeLabel.Text) != valueCount)
+                        {
+                            return false;
+                        }
+                    }
+                    else
+                    {
+                        if(i!=nRowColLen-1 || j!= nRowColLen-1)
+                        {
+                            return false;
+                        }
+                    }
+                    
+                }
+            }
+            return true;
+        }
+
+        private void btnInitialize_Click(object sender, EventArgs e)
+        {
+            int valueCount = 0;
+            for (int i = 0; i < nRowColLen; i++)
+            {
+                for (int j = 0; j < nRowColLen; j++)
+                {
+                    Label theLabel = (Label)tableLayoutPanel1.GetControlFromPosition(j, i);
+                    theLabel.Text = (++valueCount).ToString();
+                }
+            }
+            Label lastLabel = (Label)tableLayoutPanel1.GetControlFromPosition(nRowColLen - 1, nRowColLen-1);
+            lastLabel.Text = "";
+            emptyLabel = lastLabel;
+        }
+
+        // Number随机数个数
+        // minNum随机数下限
+        // maxNum随机数上限
+        public int[] GetRandomArray(int Number, int minNum, int maxNum)
+        {
+            int j;
+            int[] b = new int[Number];
+            Random r = new Random();
+            for (j = 0; j < Number; j++)
+            {
+                int i = r.Next(minNum, maxNum + 1);
+                int num = 0;
+                for (int k = 0; k < j; k++)
+                {
+                    if (b[k] == i)
+                    {
+                        num = num + 1;
+                    }
+                }
+                if (num == 0)
+                {
+                    b[j] = i;
+                }
+                else
+                {
+                    j = j - 1;
+                }
+            }
+            return b;
+        }
+
+        private void btnRandom_Click(object sender, EventArgs e)
+        {
+            int[] randomArray = new int[nRowColLen * nRowColLen];
+            randomArray = GetRandomArray(nRowColLen * nRowColLen, 1, nRowColLen * nRowColLen);
+            for (int i = 0; i < nRowColLen; i++)
+            {
+                for (int j = 0; j < nRowColLen; j++)
+                {
+                    Label theLabel = (Label)tableLayoutPanel1.GetControlFromPosition(j, i);
+                    theLabel.Text = randomArray[i * nRowColLen + j].ToString();
+                    if(theLabel.Text == (nRowColLen * nRowColLen).ToString())
+                    {
+                        theLabel.Text = "";
+                        emptyLabel = theLabel;
+                    }
+                }
+            }
+        }
+
+    }
+}
